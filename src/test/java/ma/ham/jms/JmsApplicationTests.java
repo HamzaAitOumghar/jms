4d42780181372d9239c36988f4dc7b40e53be5f6
HAMZA AIT OUMGHAR <hamza.aitoumghar.3@gmail.com>
2021-07-07 09:25:39 +0100

    first commit

---
+++ src/test/java/ma/ham/jms/JmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ham.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
